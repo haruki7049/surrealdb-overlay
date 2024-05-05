4962dbf707fd0abf3086e9af85c1496d320a42b8
haruki7049 <tontonkirikiri@gmail.com>
2024-05-05 12:10:40 +0900

    chore: Init hash

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ self: super: {
 
       src = super.fetchurl {
         url = "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-amd64.tgz";
-        hash = "";
+        hash = "sha256-JaHfiAZFgKP5RS0GCQBakYKHPnIqOtds1J65yTznGoI=";
       };
 
       installPhase = ''
