37763fe8ba5551328a2683fc27a69329f7c3893f
haruki7049 <tontonkirikiri@gmail.com>
2024-05-05 18:57:07 +0900

    chore: nix fmt

--- default.nix
+++ default.nix
@@ -2,21 +2,20 @@ self: super: {
   surrealdb = {
     "1.4.2" = let
       inherit (super.stdenv.hostPlatform) system;
-      rpath = super.lib.makeLibraryPath [
-        super.pkgs.gcc-unwrapped
-      ];
-    in
-    super.stdenv.mkDerivation rec {
+      rpath = super.lib.makeLibraryPath [ super.pkgs.gcc-unwrapped ];
+    in super.stdenv.mkDerivation rec {
       pname = "surrealdb";
       version = "1.4.2";
 
       src = {
         x86_64-linux = super.fetchurl {
-          url = "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-amd64.tgz";
+          url =
+            "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-amd64.tgz";
           hash = "sha256-JaHfiAZFgKP5RS0GCQBakYKHPnIqOtds1J65yTznGoI=";
         };
         aarch64-linux = super.fetchurl {
-          url = "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-arm64.tgz";
+          url =
+            "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-arm64.tgz";
           hash = "sha256-hlMtgEaonW41TTd2Ilrx3oXY5mdnZjfccPmg4x/6qnU=";
         };
       }.${system};
@@ -38,7 +37,8 @@ self: super: {
       '';
 
       meta = with super.lib; {
-        description = "A scalable, distributed, collaborative, document-graph database, for the realtime web";
+        description =
+          "A scalable, distributed, collaborative, document-graph database, for the realtime web";
         homepage = "https://surrealdb.com/";
         mainProgram = "surrealdb";
         license = licenses.bsl11;
