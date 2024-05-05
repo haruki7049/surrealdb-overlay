08ae3773fcf8cc8a126be530584a49ebf6c17684
haruki7049 <tontonkirikiri@gmail.com>
2024-05-05 12:07:36 +0900

    chore: Change to binary install

--- default.nix
+++ default.nix
@@ -4,36 +4,19 @@ self: super: {
       pname = "surrealdb";
       version = "1.4.2";
 
-      src = super.fetchFromGitHub {
-        owner = "surrealdb";
-        repo = "surrealdb";
-        rev = "v${version}";
-        hash = "sha256-LC/T+TkHdZ0uWaVQpR2Q9l0fShPL871rOfxxsXyJnvw=";
+      src = super.fetchurl {
+        url = "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-amd64.tgz";
+        hash = "";
       };
 
-      cargoHash = "sha256-1/l++iXiPCIwAVE8VQCSS++/9i3Kh1RNq3WnUjRy2fU=";
+      installPhase = ''
+        runHook preInstall
 
-      RUSTFLAGS = "--cfg surrealdb_unstable";
+        mkdir -p $out/bin
+        cp surreal $out/bin/surreal
 
-      nativeBuildInputs = [ super.pkg-config super.rustPlatform.bindgenHook ];
-
-      buildInputs = [ super.openssl ] ++ super.lib.optionals super.stdenv.isDarwin
-        [ super.darwin.app_sdk.frameworks.SystemConfiguration ];
-
-      doChech = false;
-
-      checkFlags = [
-        "--skip=ws_integration::none::merge"
-        "--skip=database_upgrade"
-      ];
-
-      PROTOC = "${super.protobuf}/bin/protoc";
-      PROTOC_INCLUDE = "${super.protobuf}/include";
-
-      ROCKSDB_INCLUDE_DIR = "${super.rocksdb}/include";
-      ROCKSDB_LIB_DIR = "${super.rocksdb}/lib";
-
-      __darwinAllowLocalNetworking = true;
+        runHook postInstall
+      '';
     };
   };
 }
