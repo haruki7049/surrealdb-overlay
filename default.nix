c5f1e40dd322b109f8565178b8c9be89d5cdd538
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-05-09 12:18:03 +0900

    chore: Merge main branch (#10)
    * chore: Init fetch-releases.py (#5)
    
    * chore: Init 1.4.2 (#1)
    
    * first commit
    
    * chore: hoge
    
    * fix: Add super
    
    * fix: Add super
    
    * chore: hoge
    
    * chore: Add hash
    
    * chore: hoge
    
    * chore: Add hash
    
    * chore: Add hash
    
    * chore: hoge
    
    * chore: hoge
    
    * chore: Merge main branch
    
    * chore: Change to binary install
    
    * chore: Change to mkDerivation
    
    * chore: Init hash
    
    * chore: hoge
    
    * chore: hoge
    
    * chore: Init an binary for aarch64-linux (#7)
    
    * chore: nix fmt
    
    * ci: Init some CIs (#9)
    
    * ci: Init some CIs
    
    * chore: Add an option
    
    * chore: Remove runner in nix-flake-check.yml
    
    * chore: Update treefmt.nix
    
    * chore(fetch-releases.yml): Use gpg
    
    * chore: Delete PASSPHRASE
    
    * chore: hoge
    
    * ci: Update sources.json
    
    * chore: Change binary name
    
    * ci: Update sources.json
    
    * ci: Update sources.json
    
    * ci: Update sources.json
    
    * ci: Update sources.json
    
    * ci: Update sources.json
    
    * ci: Update sources.json
    
    * ci: Update sources.json
    
    ---------
    
    Co-authored-by: haruki7049 <haruki7049@users.noreply.github.com>

---
+++ default.nix
@@ -0,0 +1,48 @@
+self: super: {
+  surrealdb = {
+    "1.4.2" = let
+      inherit (super.stdenv.hostPlatform) system;
+      rpath = super.lib.makeLibraryPath [ super.pkgs.gcc-unwrapped ];
+    in super.stdenv.mkDerivation rec {
+      pname = "surrealdb";
+      version = "1.4.2";
+
+      src = {
+        x86_64-linux = super.fetchurl {
+          url =
+            "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-amd64.tgz";
+          hash = "sha256-JaHfiAZFgKP5RS0GCQBakYKHPnIqOtds1J65yTznGoI=";
+        };
+        aarch64-linux = super.fetchurl {
+          url =
+            "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-arm64.tgz";
+          hash = "sha256-hlMtgEaonW41TTd2Ilrx3oXY5mdnZjfccPmg4x/6qnU=";
+        };
+      }.${system};
+
+      sourceRoot = ".";
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/bin
+        cp surreal $out/bin/surreal
+
+        runHook postInstall
+      '';
+
+      postFixup = ''
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/surreal" || true
+        patchelf --set-rpath ${rpath} "$out/bin/surreal" || true
+      '';
+
+      meta = with super.lib; {
+        description =
+          "A scalable, distributed, collaborative, document-graph database, for the realtime web";
+        homepage = "https://surrealdb.com/";
+        mainProgram = "surreal";
+        license = licenses.bsl11;
+      };
+    };
+  };
+}
