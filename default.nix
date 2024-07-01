a5a100c66b53209303ecb92143938f228ecb49a5
haruki7049 <tontonkirikiri@gmail.com>
2024-07-01 11:58:26 +0900

    chore: Edit mkBinaryInstall

--- default.nix
+++ default.nix
@@ -1,23 +1,15 @@
 self: super:
 let
-  inherit (super.stdenv.hostPlatform) system;
+  lib = super.lib;
+  fetchurl = super.fetchurl;
   rpath = super.lib.makeLibraryPath [ super.pkgs.gcc-unwrapped ];
   mkBinaryInstall = { pname ? "surrealdb", version, url, sha256 }:
     super.stdenv.mkDerivation rec {
       inherit pname version;
 
-      src = {
-        x86_64-linux = super.fetchurl {
-          url =
-            "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-amd64.tgz";
-          hash = "sha256-JaHfiAZFgKP5RS0GCQBakYKHPnIqOtds1J65yTznGoI=";
-        };
-        aarch64-linux = super.fetchurl {
-          url =
-            "https://github.com/surrealdb/surrealdb/releases/download/v${version}/surreal-v${version}.linux-arm64.tgz";
-          hash = "sha256-hlMtgEaonW41TTd2Ilrx3oXY5mdnZjfccPmg4x/6qnU=";
-        };
-      }.${system};
+      src = fetchurl {
+        inherit url sha256;
+      };
 
       sourceRoot = ".";
 
@@ -35,7 +27,7 @@ let
         patchelf --set-rpath ${rpath} "$out/bin/surreal" || true
       '';
 
-      meta = with super.lib; {
+      meta = with lib; {
         description =
           "A scalable, distributed, collaborative, document-graph database, for the realtime web";
         homepage = "https://surrealdb.com/";
