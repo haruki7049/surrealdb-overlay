cdd756ba0da9bc82dbb929dd0ebe489f9a4c6b2e
haruki7049 <tontonkirikiri@gmail.com>
2024-10-20 21:30:40 +0900

    fuga

--- flake.nix
+++ flake.nix
@@ -18,20 +18,7 @@
         inputs.treefmt-nix.flakeModule
       ];
 
-      flake.overlays.surrealdb-overlay =
-        self: super:
-        let
-          mkBinaryInstall = super.callPackage ./nix/mkBinaryInstall.nix { };
-        in
-        {
-          surrealdb = {
-            "1.4.2" = mkBinaryInstall {
-              version = "1.4.2";
-              url = "https://github.com/surrealdb/surrealdb/releases/download/v1.4.2/surreal-v1.4.2.linux-amd64.tgz";
-              sha256 = "10hswwyckfcysindffiaf8z8g0lib800j1id8pws70250s4dz895";
-            };
-          };
-        };
+      flake.overlays.surrealdb-overlay = import ./.;
 
       perSystem =
         { pkgs, ... }:
